dbccc74d349004493f37a65b37073728410f3c95
garrethdottin <garreth.dottin@gmail.com>
2014-01-22 00:51:14 -0800

    changes to structure to code

--- sample.js
+++ sample.js
@@ -1,25 +1,33 @@
-//
-//
-//
+//check the values of the selling price
+//change to int
+//Fill in the form if a certain criterium is met
+//Reload the page
 
 
 var casper = require('casper').create();
-
 casper.start('https://btc-e.com/exchange/ltc_btc');
 
 casper.then(function() {
-  var currentPrice = this.evaluate(function() {
+  var currentSellingPrice = this.evaluate(function() {
     return document.querySelector('#orders_1 .table tr:nth-child(2) td:first-child').textContent;
   });
 
-  var currentVolume = this.evaluate(function() {
+  var currentLtcVol = this.evaluate(function() {
     return document.querySelector('#orders_1 .table tr:nth-child(2) td:nth-child(2)').textContent;
 
   });
-  this.echo("The price is " + currentVolume);
-  this.echo("The volume at that price " + currentVolume)
+
+  this.echo("The price is " + currentSellingPrice);
+  this.echo("The LTC volume at that price " + currentLtcVol)
+  this.echo("the ")
 
 });
+casper.then(function() {
+  f
+
+
+})
+
 
 
 casper.run();
