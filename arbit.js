02e2e42c5ef1a12074bd81d9e3d94890e0431cce
garrethdottin <garreth.dottin@gmail.com>
2014-02-10 01:09:19 -0800

    value updating form, cannot click button still

--- arbit.js
+++ arbit.js
@@ -1,7 +1,6 @@
 var casper = require('casper').create({
   waitTimeout: 15000,
-  stepTimeout: 10000,
-  // logLevel: "debug",
+  logLevel: "debug",
   verbose: true,
   pageSettings: {
     userAgent: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.10 (KHTML, like Gecko) Chrome/23.0.1262.0 Safari/537.10',
@@ -21,14 +20,14 @@ var domMunipulations = {
   getLTCValue: function () {
     return $('.table:first-child .order:nth-child(2) td:first-child')[0].innerHTML;
   },
-  setAmount: function(amount) {
-    return $('.tabla2 tr td:nth-child(2) #b_btc').val(amount);
+  setAmount: function() {
+    return $('.tabla2 tr td:nth-child(2) #b_btc').val(5)
   },
   clickCalculateButton: function() {
-    return $('a.button:first-child')[0];
+    return $('a.button:first-child')[0]
   },
   clickBuyButton: function () {
-    return $('a.button')[1];
+    return $('a:contains("Buy LTC")')[0]
   }
 }
 
@@ -37,7 +36,8 @@ casper.start('https://btc-e.com/exchange/ltc_btc'); //+++ change the url
 casper.then(function (currentTime) {
   for (customizedVariables.currentTime = 0; customizedVariables.currentTime < customizedVariables.timer; customizedVariables.currentTime++) {
     this.waitFor(function check() {
-      return this.grabLTCValue();
+      // return this.grabLTCValue();
+      return this.buyLTC();
     });
   }
 });
@@ -81,14 +81,40 @@ casper.checktoBuy = function (ltcPrice,Highpoint, priceDrop) {
 
 // Function to buy
 casper.buyLTC = function () {
+
+  //This evalaute only working sometimes in the current state
+  //evaluate needs to be set to a variable
+  //Try doing the calculate button without the 0
+  // also use a wait for
+
   //Need a function to set amount to buy
-  var amountLtc = this.evaluate(domMunipulations.setAmount(5))
-  var clickCalculate = this.evaluate(domMunipulations.clickCalculate)
-  var clickBuyButton = this.evaluate(domMunipulations.clickBuyButton)
-  this.click(clickCalculate)
-  this.wait(500, function (clickBuyButton) {
-    this.click(clickBuyButton)
-  });
+  var amountLtc = this.evaluate(domMunipulations.setAmount)
+  this.capture('attempt1.png', {
+        top: 100,
+        left: 100,
+        width: 500,
+        height: 1000
+    });
+
+  links = this.evaluate(function() {
+    return ("this part is working")
+
+    // this.click($('a:contains("Buy LTC")')[0])
+    // this.capture('attempt2.png', {
+    //     top: 100,
+    //     left: 100,
+    //     width: 500,
+    //     height: 400
+    // });
+  })
+
+  this.echo(links)
+  // var clickCalculate = this.evaluate(domMunipulations.clickCalculateButton)
+  // var clickBuyButton = this.evaluate(domMunipulations.clickBuyButton)
+
+  // // this.wait(500, function (clickBuyButton) {
+  //     this.click(clickBuyButton)
+  // // });
 };
 // Function to sell
 casper.sellLTC = function () {
